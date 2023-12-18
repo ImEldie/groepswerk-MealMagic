46367dec24737ac9f11e05a915ab3002c27bc900
ImEldie <daniel.frankort@telenet.be>
2023-12-18 13:21:17 +0100

    Initial Commit & Basislayout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: MainLayoutComponent,
+        children: [
+        {
+            path: "",
+            loadComponent: () =>
+                import("./pages/homepage/homepage.component").then(
+                    (c) => c.HomepageComponent
+                ),
+        },
+        ]
+    }
+];
---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, MatToolbarModule],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css'
+})
+export class MainLayoutComponent {
+
+}
