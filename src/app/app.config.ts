aa32e6c74c80757314e6b1592e2e588959fd3dbd
AleArrigucci <arrigucci.a@gmail.com>
2023-12-20 11:30:21 +0100

    Added withhash + provideHttp

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withHashLocation, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withHashLocation()),
+    provideAnimations(),
+    provideHttpClient(withFetch()),
+  ],
   providers: [
     provideRouter(routes, withHashLocation()),
     provideAnimations(),
