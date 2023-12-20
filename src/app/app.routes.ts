f6dab4db553e2313d0ae15f978d24e25294399e9
AleArrigucci <arrigucci.a@gmail.com>
2023-12-20 11:29:23 +0100

    Added route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,24 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
-    {
-        path: "",
-        component: MainLayoutComponent,
-        children: [
-        {
-            path: "",
-            loadComponent: () =>
-                import("./pages/homepage/homepage.component").then(
-                    (c) => c.HomepageComponent
-                ),
-        },
-        ]
-    }
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/homepage/homepage.component').then(
+            (c) => c.HomepageComponent
+          ),
+      },
+      {
+        path: 'userpanel',
+        loadComponent: () =>
+          import('./pages/userpanel/userpanel.component').then(
+            (c) => c.UserpanelComponent
+          ),
+      },
+    ],
+  },
 ];
