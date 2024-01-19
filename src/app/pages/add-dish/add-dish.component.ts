c24c44b58006c4ebb53054ccb6a8e7bc32346851
ImEldie <daniel.frankort@telenet.be>
2024-01-19 15:22:03 +0100

    undefined-check in postdish

--- src/app/pages/add-dish/add-dish.component.ts
+++ src/app/pages/add-dish/add-dish.component.ts
@@ -28,20 +28,21 @@ export class AddDishComponent {
   ){}
 
   postDish(){
-    if (this.userInputsValid() && this.allUserInputsDefined()) {
-      const postData: DishPostData = {
-        name: this.nameInputData!.name,
-        description: this.nameInputData!.description,
-        image_url: this.nameInputData!.image,
-        duration: this.detailInputData!.prepTime,
-        amount_of_people: this.detailInputData!.portionSize,
-        season_id: this.detailInputData!.seasonId,
-        ingredients: this.selectedIngredients!.ingredientIds,
-        dish_types: this.detailInputData!.selectedTypeIds,
-        dish_steps: [],
-      }     
-
-      this.dishApi.postNewDish(postData, this.createdSteps!.createdSteps);
+    if (this.userInputsValid()) {
+      if (this.nameInputData !== undefined && this.detailInputData !== undefined && this.selectedIngredients !== undefined && this.createdSteps !== undefined) {
+        const postData: DishPostData = {
+          name: this.nameInputData.name,
+          description: this.nameInputData.description,
+          image_url: this.nameInputData.image,
+          duration: this.detailInputData.prepTime,
+          amount_of_people: this.detailInputData.portionSize,
+          season_id: this.detailInputData.seasonId,
+          ingredients: this.selectedIngredients.ingredientIds,
+          dish_types: this.detailInputData.selectedTypeIds,
+          dish_steps: [],
+        }     
+        this.dishApi.postNewDish(postData, this.createdSteps!.createdSteps);
+      }
     }
   }
 
