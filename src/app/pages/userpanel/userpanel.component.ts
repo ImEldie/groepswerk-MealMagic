8e8a44ee6ec3af8a1c4564f9d8999192aafa7b6e
ImEldie <daniel.frankort@telenet.be>
2024-01-19 14:16:58 +0100

    placed userpanel to /api-calls

--- src/app/pages/userpanel/userpanel.component.ts
+++ src/app/pages/userpanel/userpanel.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
-import { UserpanelService } from '../../services/userpanel-service/userpanel.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
@@ -26,6 +25,7 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { Observable, map } from 'rxjs';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { UserpanelService } from '../../services/api-calls/userpanel.service';
 @Component({
   selector: 'app-userpanel',
   standalone: true,
