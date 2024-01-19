3405722f5e3c382f09a9ed76ec01eddcb1cdd6b5
AleArrigucci <arrigucci.a@gmail.com>
2024-01-19 14:58:59 +0100

    add interfaces

--- src/app/interfaces/fridge-interface.ts
+++ src/app/interfaces/fridge-interface.ts
@@ -1,2 +1,16 @@
 export interface Fridge {
+  id: number;
+  user_detail_id: number;
+}
+export interface FridgeRespone {
+  data: Array<Fridge>;
+}
+export interface FridgeIngredientResponse {
+  data: Array<FridgeIngredient>;
+}
+export interface FridgeIngredient {
+  id: number;
+  ingredient_id: number;
+  fridge_id: number;
+  amount: number;
 }
