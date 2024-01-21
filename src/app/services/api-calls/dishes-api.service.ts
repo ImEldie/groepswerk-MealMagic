3a760a1ee31f7db70ce3c461e906d6f1aec751a5
AleArrigucci <arrigucci.a@gmail.com>
2024-01-21 16:11:52 +0100

    prettier + del extra comma

--- src/app/services/api-calls/dishes-api.service.ts
+++ src/app/services/api-calls/dishes-api.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Dish, DishApiResponse, DishPostData } from '../../interfaces/interfaces-dishes';
+import {
+  Dish,
+  DishApiResponse,
+  DishPostData,
+} from '../../interfaces/interfaces-dishes';
 import { Observable, forkJoin, map } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { StepsApiService } from './steps-api.service';
@@ -16,9 +20,9 @@ export class DishesApiService {
 
   constructor(
     private http: HttpClient,
-    private auth: AuthService,,
+    private auth: AuthService,
     private stepsApi: StepsApiService,
-    private router: Router
+    private router: Router,
   ) {}
 
   loadDishesFromApi(): void {
@@ -38,50 +42,49 @@ export class DishesApiService {
         console.log(this.dishes);
       });
   }
-  getDishList(): Array<Dish>{
+  getDishList(): Array<Dish> {
     return this.dishes;
   }
 
-  postNewDish(postData: DishPostData, stepsToPost: Array<DishStep>){
-    const stepsObservables: Array<Observable<Step>> = this.stepsApi.postDishSteps(stepsToPost);
+  postNewDish(postData: DishPostData, stepsToPost: Array<DishStep>) {
+    const stepsObservables: Array<Observable<Step>> =
+      this.stepsApi.postDishSteps(stepsToPost);
 
-    forkJoin(stepsObservables).subscribe(
-      (result: Array<Step>) => {
-        result.sort((a, b) => a.order - b.order);
-        postData.dish_steps = result.map((step) => step.id);
+    forkJoin(stepsObservables).subscribe((result: Array<Step>) => {
+      result.sort((a, b) => a.order - b.order);
+      postData.dish_steps = result.map((step) => step.id);
 
-        this.postDish(postData);
-      },
-    )
+      this.postDish(postData);
+    });
   }
-  private postDish(postData: DishPostData){
-    const targetLink = "https://syntra2023.code-coaching.dev/api/group-2/dishes/";
+  private postDish(postData: DishPostData) {
+    const targetLink =
+      'https://syntra2023.code-coaching.dev/api/group-2/dishes/';
     const token = this.auth.getBearerToken();
 
     this.http
-    .post<DishPostData>(targetLink, 
-      {
-        name: postData.name,
-        image_url: postData.image_url,
-        description: postData.description,
-        duration: postData.duration,
-        amount_of_people: postData.amount_of_people,
-        season_id: postData.season_id,
-        ingredients: postData.ingredients,
-        dish_types: postData.dish_types,
-        dish_steps: postData.dish_steps
-      },
-      {
-        headers: new HttpHeaders(
-          { Authorization: "Bearer " + token }
-        )
-      }
-    )
-    .subscribe((d) => {
+      .post<DishPostData>(
+        targetLink,
+        {
+          name: postData.name,
+          image_url: postData.image_url,
+          description: postData.description,
+          duration: postData.duration,
+          amount_of_people: postData.amount_of_people,
+          season_id: postData.season_id,
+          ingredients: postData.ingredients,
+          dish_types: postData.dish_types,
+          dish_steps: postData.dish_steps,
+        },
+        {
+          headers: new HttpHeaders({ Authorization: 'Bearer ' + token }),
+        },
+      )
+      .subscribe((d) => {
         this.router.navigate(['']);
       });
   }
-  convertToIdArray(arrayToConvert: Array<Step|Ingredient>): Array<number> {
+  convertToIdArray(arrayToConvert: Array<Step | Ingredient>): Array<number> {
     let idArray: Array<number> = arrayToConvert.map((data) => data.id);
     return idArray;
   }
