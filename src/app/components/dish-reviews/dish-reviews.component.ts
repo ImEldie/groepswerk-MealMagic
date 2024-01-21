c1d125b72f52d6a6e282f72128100f96fcecbafe
AleArrigucci <arrigucci.a@gmail.com>
2024-01-21 18:29:52 +0100

    create component

---
+++ src/app/components/dish-reviews/dish-reviews.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-reviews',
+  standalone: true,
+  imports: [],
+  templateUrl: './dish-reviews.component.html',
+  styleUrl: './dish-reviews.component.css'
+})
+export class DishReviewsComponent {
+
+}
